0c2de64120b313f24a23b55c7b6f07aeb490dccb
jdeeny <jdeeny@gmail.com>
2019-01-03 20:59:49 -0700

    ai improved but shitty

--- ai/init.lua
+++ ai/init.lua
@@ -4,96 +4,174 @@ local AiPlayer = class('AiPlayer')
 
 function AiPlayer:initialize()
   self.customers = {}
+  self.buttons = {
+  }
+  self.commands = {}
+  self.debounce = false
+  self.laststate = false
+  self.release_s = false
 end
 
-function AiPlayer:update(dt)
-end
+function AiPlayer:update()
+  local fresh = false
 
-
-function AiPlayer:setCustomers(customers)
-  self.customers = customers
-end
-
-function AiPlayer:importantDrinks()
-
-end
-
-
-
--- Act like a controller
-function AiPlayer:pressed(control)
-  local n = '_handle_release_' .. control
-  if self[n] then
-    return self[n]()
-  else
-    return false
+  local state = gameWorld.gameState:getCurrent()
+  if state ~= laststate then
+    laststate = state
+    fresh = true
   end
 
-end
+  if state == 'splash' then
+    if fresh then
+      self.buttons = {}
+      self:delaySet(0.2)
+    end
+    if self:delayComplete() then
+      self:pushCommand({ 'action', })
+      self:delaySet(0.2)
+    end
+  elseif state == 'title' then
+    if fresh then
+      self.buttons = {}
+      self:delaySet(0.2)
+    end
+    if self:delayComplete() then
+      self:delaySet(0.2)
+      local sel = gameWorld.gameState:getState().menu.selected
+      print(sel)
+      if sel < 2 then
+        self:pushCommand({'down'})
+      elseif sel > 2 then
+        self:pushCommand({'up'})
+      else
+        self:pushCommand({'action'})
+      end
+    end
 
-function AiPlayer:released(control)
-  local n = '_handle_' .. control
-  if self[n] then
-    return self[n]()
-  else
-    return false
+  elseif state == 'tutorial' then
+    if fresh then
+      self.buttons = {}
+      self:delaySet(0.2)
+    end
+    if self:delayComplete() then
+      self:delaySet(0.2)
+      self:pushCommand('action')
+    end
+  elseif state == 'gameplay' then
+    if fresh then
+      self.release_s = 0.2
+      self.buttons = {}
+      self.served = {}
+      self:delaySet(0.5)
+    end
+
+    local patrons = gameWorld.gameState:getState().patrons
+
+    if self:delayComplete() and #patrons > 0 then
+      local drinkmix = patrons[1].drinkMix
+      self:makeDrink(drinkmix)
+      self:deliverDrink(patrons[1].row)
+      --pretty.dump(drinkmix)
+      self:delayStop()
+    end
+
+
+  elseif state == 'ending' then
+  elseif state == 'credits' then
+    if fresh then
+      self:pushCommand({'action'})
+    end
   end
 
-end
-
-function AiPlayer:_handle_release_action()
-  return true
-end
-
-
-
-function AiPlayer:_handle_action()
-  if gameWorld.gameState:getCurrent() == 'splash' then return true end
-  if gameWorld.gameState:getCurrent() == 'title' then
-    if gameWorld.gameState:getState().menu.selected == 2 then return true end
+  if not self.release_s or self.release_delay and self.releasedelay <= love.timer.getTime() then
+    self.buttons = {}
+    self.releasedelay = false
   end
-  if gameWorld.gameState:getCurrent() == 'tutorial' then return true end
-
-  return false
-end
-
-function AiPlayer:_handle_pour()
-  return false
-end
-
-function AiPlayer:_handle_swap()
-  return false
-end
-
-function AiPlayer:_handle_pause()
-  return false
-end
-
-
-function AiPlayer:_handle_up()
-  if gameWorld.gameState:getCurrent() == 'title' then
-    if gameWorld.gameState:getState().menu.selected > 2 then return true end
+  if not self.commanddelay or self.commanddelay < love.timer.getTime() then
+    local c = self:nextCommand()
+    --self.releasedelay = love.timer.getTime() + 0.0005
+    self.commanddelay = love.timer.getTime() + 0.4
+    if type(c) == 'string' then
+      self.buttons[c] = true
+    else
+      for _, k in ipairs(c) do
+        self.buttons[k] = true
+      end
+    end
+    if self.buttons.up or self.buttons.down then
+      self.releasedelay = love.timer.getTime() + (self.release_s or 0.0005)
+    end
   end
-
-  return false
 end
 
-function AiPlayer:_handle_down()
-  if gameWorld.gameState:getCurrent() == 'title' then
-    if gameWorld.gameState:getState().menu.selected < 2 then return true end
+function AiPlayer:makeDrink(drink)
+  if drink.b then self:pushCommand{'pour'} end
+  if drink.a then
+    self:pushCommand('up')
+    self:pushCommand('pour')
+    self:pushCommand('down')
   end
-
-
-  return false
+  if drink.c then
+    self:pushCommand('down')
+    self:pushCommand('pour')
+    self:pushCommand('up')
+  end
 end
 
-function AiPlayer:_handle_left()
-  return false
+function AiPlayer:deliverDrink(row)
+  if row == 2 then self:pushCommand{'action'} end
+  if row == 1 then
+    self:pushCommand('up')
+    self:pushCommand('action')
+    self:pushCommand('down')
+  end
+  if row == 3 then
+    self:pushCommand('down')
+    self:pushCommand('action')
+    self:pushCommand('up')
+  end
 end
 
-function AiPlayer:_handle_right()
-  return false
+
+function AiPlayer:delaySet(s)
+  self.delay = love.timer.getTime() + 0.5
 end
+
+function AiPlayer:delayComplete()
+  if not self.delay then return false end
+  return self.delay < love.timer.getTime()
+end
+
+function AiPlayer:delayStop()
+  self.delay = false
+end
+
+function AiPlayer:pushCommand(command)
+  table.insert(self.commands, command)
+end
+
+function AiPlayer:nextCommand(command)
+  local c = self.commands[1]
+  if not c then return {} end
+  table.remove(self.commands, 1)
+  return c
+end
+
+
+function AiPlayer:pressed(key)
+  if not self.buttons[key] then
+    self.buttons[key] = false
+  end
+  return self.buttons[key]
+end
+
+function AiPlayer:released(key)
+  if not self.buttons[key] then
+    self.buttons[key] = false
+  end
+  return not self.buttons[key]
+end
+
 
 
 return AiPlayer
